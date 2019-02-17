549aad0da43cd434da798e66f982b5091911edbc
Chris <chrisjones0517@gmail.com>
2019-02-17 11:46:03 -0600

    changed port

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is listening on port 3000');
 });
